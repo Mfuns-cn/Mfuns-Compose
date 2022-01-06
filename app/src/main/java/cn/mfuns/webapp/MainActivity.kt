7cc4ced57e3455b9d40644ed6a280f3085417941
Il Harper <afanyiyu@hotmail.com>
2022-01-07 01:28:09 +0800

    feat: webView update

--- app/src/main/java/cn/mfuns/webapp/MainActivity.kt
+++ app/src/main/java/cn/mfuns/webapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package cn.mfuns.webapp
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(WebViewContainer.webView)
+    }
+
+    override fun onBackPressed() {
+        if (WebViewContainer.webView != null && WebViewContainer.webView!!.canGoBack())
+            WebViewContainer.webView!!.goBack()
+        else
+            super.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        if (WebViewContainer.webView != null) {
+            WebViewContainer.webView!!.loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
+            WebViewContainer.webView!!.clearHistory()
+            (WebViewContainer.webView!!.parent as ViewGroup).removeView(WebViewContainer.webView)
+            // WebViewContainer.webView!!.destroy()
+            WebViewContainer.webView = null
+        }
+        super.onDestroy()
     }
 }
--- app/src/main/java/cn/mfuns/webapp/WebViewContainer.kt
+++ app/src/main/java/cn/mfuns/webapp/WebViewContainer.kt
@@ -2,6 +2,9 @@ package cn.mfuns.webapp
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import java.util.*
@@ -14,12 +17,42 @@ class WebViewContainer {
         private val listeners: HashSet<WebViewContainerInitializedListener> =
             HashSet<WebViewContainerInitializedListener>()
 
+        @SuppressLint("SetJavaScriptEnabled")
         fun initialize(context: Context, listener: WebViewContainerInitializedListener) {
             listeners += listener
 
             if (webView != null) notifyInitialized()
 
             webView = WebView(context.applicationContext)
+
+            // Configure
+            val settings = webView!!.settings
+            settings.javaScriptEnabled = true
+            settings.useWideViewPort = true
+            settings.loadWithOverviewMode = true
+            settings.setSupportZoom(false)
+            settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.loadsImagesAutomatically = true
+            settings.defaultTextEncodingName = "utf-8"
+            settings.saveFormData = true
+            settings.savePassword = true
+
+            // Cache
+            settings.setAppCacheEnabled(true)
+            settings.domStorageEnabled = true
+            settings.databaseEnabled = true
+
+            // Handle back
+            webView!!.setOnKeyListener(object : View.OnKeyListener{
+                override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+                    if (keyCode == KeyEvent.KEYCODE_BACK && webView!!.canGoBack()) {
+                        webView!!.goBack()
+                        return true
+                    }
+                    return false
+                }
+            })
 
             // Listen onPageFinished()
             webView!!.webViewClient = object : WebViewClient() {
