bda9717e7bf53e7fcb6e094cf48a35979f39e8f6
Il Harper <afanyiyu@hotmail.com>
2022-01-21 08:45:34 +0800

    refactor: update SplashActivity

--- app/src/main/java/cn/mfuns/webapp/SplashActivity.kt
+++ app/src/main/java/cn/mfuns/webapp/SplashActivity.kt
@@ -1,7 +1,6 @@
 package cn.mfuns.webapp
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -30,21 +29,18 @@ class SplashActivity : AppCompatActivity() {
         setContentView(image)
 
         // Adapt to full screen
-        val decorView = window.decorView
-        val option = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            or View.SYSTEM_UI_FLAG_FULLSCREEN)
-        decorView.systemUiVisibility = option
-        window.statusBarColor = Color.TRANSPARENT
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val layoutParams = window.attributes
+        window.decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-                layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
-            else layoutParams.layoutInDisplayCutoutMode =
+                window.attributes.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+            else window.attributes.layoutInDisplayCutoutMode =
                 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-            window.attributes = layoutParams
-        }
     }
 
     override fun onResume() {
