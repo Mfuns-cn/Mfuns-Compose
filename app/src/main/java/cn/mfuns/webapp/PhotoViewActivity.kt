3a11b94b2f97e2dd8c5eb1653fa1fa5fa3a87f82
Il Harper <afanyiyu@hotmail.com>
2022-02-02 14:32:31 +0800

    feat: update PhotoViewActivity

--- app/src/main/java/cn/mfuns/webapp/PhotoViewActivity.kt
+++ app/src/main/java/cn/mfuns/webapp/PhotoViewActivity.kt
@@ -1,26 +1,18 @@
 package cn.mfuns.webapp
 
-import android.app.Activity
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import cn.mfuns.webapp.databinding.ActivityPhotoViewBinding
-import cn.mfuns.webapp.util.DownloadRequest
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.Volley
+import cn.mfuns.webapp.util.Viewer
 import java.io.File
 
 class PhotoViewActivity : AppCompatActivity() {
-    companion object {
-        private var queue: RequestQueue? = null
-    }
 
     private lateinit var binding: ActivityPhotoViewBinding
-    private lateinit var uri: Uri
+    private lateinit var file: Triple<Uri, String, File>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +32,11 @@ class PhotoViewActivity : AppCompatActivity() {
         binding.actionBar.setOnMenuItemClickListener { item ->
             when (item.itemId) {
                 R.id.viewer_action_photo_open -> {
-                    PhotoViewActions.open(this, uri)
+                    Viewer.open(this, file.first)
                     true
                 }
                 R.id.viewer_action_photo_download -> {
-                    PhotoViewActions.download(this, uri)
+                    Viewer.save(this, file.third, file.second)
                     true
                 }
                 else -> false
@@ -52,52 +44,23 @@ class PhotoViewActivity : AppCompatActivity() {
         }
 
         // Download Photo
-        Runnable {
-            // Check URL
-            val url = intent.getStringExtra("url")
-            if (url.isNullOrBlank()) {
+        val url = intent.getStringExtra("url")
+        if (url == null) {
+            Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        Viewer.download(
+            this,
+            url!!,
+            { f ->
+                file = f
+                binding.photoView.setImageURI(file.first)
+                binding.loadingBar.isIndeterminate = false
+            },
+            {
                 Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
                 finish()
             }
-            val filename = url!!.split('/').last()
-
-            // Create temp file
-            val viewerProviderFolder = File(cacheDir, "viewerprovider")
-            viewerProviderFolder.mkdirs()
-            val file = File(viewerProviderFolder, filename)
-
-            // Make download request
-            if (queue == null) queue = Volley.newRequestQueue(this)
-            val request = DownloadRequest(url, { response ->
-                run {
-                    file.writeBytes(response)
-                    uri = FileProvider.getUriForFile(this, "cn.mfuns.webapp.viewerprovider", file)
-                    binding.photoView.setImageURI(uri)
-                    binding.loadingBar.isIndeterminate = false
-                }
-            }, {
-                Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
-                finish()
-            })
-
-            // Enqueue request
-            queue!!.add(request)
-        }.run()
-    }
-}
-
-class PhotoViewActions {
-    companion object {
-        fun open(activity: Activity, uri: Uri) {
-            Intent(Intent.ACTION_VIEW).apply {
-                data = uri
-                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                if (resolveActivity(activity.packageManager) != null) activity.startActivity(this)
-            }
-        }
-
-        fun download(activity: Activity, uri: Uri) {
-            TODO()
-        }
+        )
     }
 }
