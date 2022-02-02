80d5c6c743570d8037aaaf666a7df1fe8c044d2f
Il Harper <afanyiyu@hotmail.com>
2022-02-02 12:26:24 +0800

    feat: add AndroidUtil

---
+++ app/src/main/java/cn/mfuns/webapp/util/AndroidUtil.kt
@@ -0,0 +1,28 @@
+package cn.mfuns.webapp.util
+
+import android.app.Activity
+
+class AndroidUtil {
+    companion object {
+        fun getCurrentActivity(): Activity {
+            val activityThreadClass = Class.forName("android.app.ActivityThread")
+            val activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null)
+            val activitiesField = activityThreadClass.getDeclaredField("mActivities").apply {
+                isAccessible = true
+            }
+            for (activityRecord in (activitiesField.get(activityThread) as Map<*, *>).values) {
+                val activityRecordClass = activityRecord!!.javaClass
+                val pausedField = activityRecordClass.getDeclaredField("paused").apply {
+                    isAccessible = true
+                }
+                if (!pausedField.getBoolean(activityRecord)) {
+                    val activityField = activityRecordClass.getDeclaredField("activity").apply {
+                        isAccessible = true
+                    }
+                    return activityField.get(activityRecord) as Activity
+                }
+            }
+            throw RuntimeException("Cannot get activity")
+        }
+    }
+}
