c352d1437fb673c9cd472f9a954ae535b7dba2dd
Il Harper <afanyiyu@hotmail.com>
2022-02-03 12:21:29 +0800

    feat: close #4

--- app/src/main/java/cn/mfuns/webapp/MainActivity.kt
+++ app/src/main/java/cn/mfuns/webapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import cn.mfuns.webapp.util.AndroidUtil.Companion.setFullscreen
@@ -62,12 +63,19 @@ class MainActivity : AppCompatActivity() {
         isInitialized = true
     }
 
+    private var backPressTime: Long = -2500
+
     override fun onBackPressed() {
         if (!isInitialized) {
             finish()
             return
         }
-        if (!WebViewContainer.defaultContainer.goBack()) super.onBackPressed()
+        if (WebViewContainer.defaultContainer.goBack()) return
+        val now = System.currentTimeMillis()
+        if (now - backPressTime > 2000) {
+            backPressTime = now
+            Toast.makeText(this, R.string.back_press_prompt, Toast.LENGTH_SHORT).show()
+        } else finish()
     }
 
     override fun onDestroy() {
