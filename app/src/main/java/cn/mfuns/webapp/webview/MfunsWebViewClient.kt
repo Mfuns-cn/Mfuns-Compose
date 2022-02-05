31e10344325a317f357ffee70f3e2c95a125b73a
Il Harper <afanyiyu@hotmail.com>
2022-02-05 08:51:38 +0800

    fix: fix #12

--- app/src/main/java/cn/mfuns/webapp/webview/MfunsWebViewClient.kt
+++ app/src/main/java/cn/mfuns/webapp/webview/MfunsWebViewClient.kt
@@ -13,52 +13,55 @@ import java.net.URL
 
 class MfunsWebViewClient(private val activity: Activity) : WebViewClient() {
     override fun shouldOverrideUrlLoading(v: WebView?, url: String): Boolean {
-        // Parse URL
-        var path = URL(url).path
-        if (path == "/") return false
-        path = url.split(path)[0] + path
-        if (path.isNullOrBlank()) return false
+        try {
+            // Parse URL
+            var path = URL(url).path
+            if (path == "/") return false
+            path = url.split(path)[0] + path
+            if (path.isNullOrBlank()) return false
 
-        if (Viewer.parseExt(path) != null) {
-            // Use Viewer
-            activity.apply {
-                when (PreferenceManager.getDefaultSharedPreferences(this).getString(
-                    "settings_viewer_default_action",
-                    resources.getStringArray(R.array.settings_viewer_action_list)[0]
-                )) {
-                    resources.getStringArray(R.array.settings_viewer_action_list)[0] -> {
-                        val intent = Intent(this, PhotoViewActivity::class.java)
-                        intent.putExtra("url", path)
-                        startActivity(intent)
-                    }
-                    resources.getStringArray(R.array.settings_viewer_action_list)[1] -> {
-                        Viewer.download(
-                            this,
-                            url,
-                            { f -> Viewer.open(this, f.first) },
-                            {
-                                Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
-                                finish()
-                            }
-                        )
-                    }
-                    resources.getStringArray(R.array.settings_viewer_action_list)[2] -> {
-                        Viewer.download(
-                            this,
-                            url,
-                            { f -> Viewer.save(this, f.third, f.second) },
-                            {
-                                Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
-                                finish()
-                            }
-                        )
+            if (Viewer.parseExt(path) != null) {
+                // Use Viewer
+                activity.apply {
+                    when (PreferenceManager.getDefaultSharedPreferences(this).getString(
+                        "settings_viewer_default_action",
+                        resources.getStringArray(R.array.settings_viewer_action_list)[0]
+                    )) {
+                        resources.getStringArray(R.array.settings_viewer_action_list)[0] -> {
+                            val intent = Intent(this, PhotoViewActivity::class.java)
+                            intent.putExtra("url", path)
+                            startActivity(intent)
+                        }
+                        resources.getStringArray(R.array.settings_viewer_action_list)[1] -> {
+                            Viewer.download(
+                                this,
+                                url,
+                                { f -> Viewer.open(this, f.first) },
+                                {
+                                    Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
+                                    finish()
+                                }
+                            )
+                        }
+                        resources.getStringArray(R.array.settings_viewer_action_list)[2] -> {
+                            Viewer.download(
+                                this,
+                                url,
+                                { f -> Viewer.save(this, f.third, f.second) },
+                                {
+                                    Toast.makeText(this, R.string.viewer_download_failed, Toast.LENGTH_SHORT).show()
+                                    finish()
+                                }
+                            )
+                        }
                     }
                 }
+
+                return true
             }
-
-            return true
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-
         return false
     }
 }
