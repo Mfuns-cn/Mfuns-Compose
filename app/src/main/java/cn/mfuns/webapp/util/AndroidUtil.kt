715a02be94412031ab729db27a9ea8c3ee497c2d
Il Harper <afanyiyu@hotmail.com>
2022-02-08 10:24:49 +0800

    feat: update Window.setFullscreen()

--- app/src/main/java/cn/mfuns/webapp/util/AndroidUtil.kt
+++ app/src/main/java/cn/mfuns/webapp/util/AndroidUtil.kt
@@ -29,19 +29,24 @@ class AndroidUtil {
             throw RuntimeException("Cannot get activity")
         }
 
-        fun Window.setFullscreen(enable: Boolean) {
+        fun Window.setFullscreen(enable: Boolean, video: Boolean = false) {
             if (enable) {
                 decorView.systemUiVisibility =
                     (View.SYSTEM_UI_FLAG_FULLSCREEN
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-                        attributes.layoutInDisplayCutoutMode =
-                            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
-                    else attributes.layoutInDisplayCutoutMode =
-                        WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or (
+                        if (video) View.KEEP_SCREEN_ON
+                        else 0
+                        ))
+                if (!video)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                            attributes.layoutInDisplayCutoutMode =
+                                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+                        else attributes.layoutInDisplayCutoutMode =
+                            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
             } else {
                 decorView.systemUiVisibility = 0
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
