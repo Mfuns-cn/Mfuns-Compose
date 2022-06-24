ecb6fb8233ffc690bac5d6da69a234c6fb2f2678
Il Harper <hi@ilharper.com>
2022-06-25 03:26:48 +0800

    chore: enable jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://maven.ilharper.com") }
     }
 }
