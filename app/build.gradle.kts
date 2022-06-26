cb44d07b1c18f6944011fb55f483dedbdadd4dad
Il Harper <hi@ilharper.com>
2022-06-26 10:07:13 +0800

    chore: enable minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ android {
 
     buildTypes {
         getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
